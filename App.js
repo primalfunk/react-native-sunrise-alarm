4585dfbf723fdab6a5531198c5d50875c9b2df79
Jared Menard <jared.d.menard@gmail.com>
2018-08-27 10:42:47 -0600

    all the functionality, terrible styling

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Alert, Button, Linking, StyleSheet, Text, View, Picker } from 'react-native'
+import { Alert, Button, Linking, StyleSheet, Text, View } from 'react-native'
 import axios from 'react-native-axios'
 import BackgroundTimer from 'react-native-background-timer'
 import moment from 'moment'
@@ -9,7 +9,7 @@ import Permissions from 'react-native-permissions'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     alignContent: 'center',
     backgroundColor: '#F5FCFF',
@@ -37,40 +37,41 @@ const styles = StyleSheet.create({
 })
 
 export default class App extends Component {
-  state = { permission: '', lat: '', long: '', alt: '', sunrise: '', selection: 'set', isSet: false, isAlarm: false }
+  state = { showAlarm: false, 
+            permission: '', 
+            lat: '', 
+            long: '', 
+            sunrise: '', 
+            selection: 'set', 
+            setDate: moment(), 
+            timer: {}, 
+            music: {}, 
+            isPlaying: false }
 
   componentDidMount() {
     this.getPosition()
   }
 
-  handleSelection = ( selection, lat, long ) => {
-    switch( selection ) {
-      case 'set':
-        this.getSunriseTime( lat, long )
-        break
-      case 'delete':
-        Alert.alert(
-          '',
-          'This will disable any alarms set by this app.',
-          [
-            { text: 'Ok', onPress: () => null },
-          ]
-        )
-        //more code
-      default:
-        console.log("default selection")
-    }
+  setSunriseAlarm = ( lat, long ) => {
+    this.getSunriseTime( lat, long )
   }
 
-  handleSetter = ( setdate ) => {
+  setAlarm = ( setDate ) => {
+    let now = moment()
+    let duration = moment.duration(setDate.diff(now))
+    let ms = duration.asMilliseconds()
     const timer = BackgroundTimer.setTimeout(() => {
-      // this will be executed once after 10 seconds
-      // even when app is the the background
-      this.setState({ isAlarm: true })
       this.makeNoise()
-    }, 10000)
-
-    this.setState({ isSet: true, sunrise: '' })
+    }, ms)
+    this.setState({ isSet: true, showAlarm: true, timer: timer })
+    Alert.alert(
+      'Sunrise alarm set',
+      `${moment(setDate).format('MMMM Do YYYY, h:mm:ss a')}`,
+      [
+        { text: 'OK', onPress: () => null },
+      ],
+      { cancelable: false }
+    )
   }
 
   makeNoise = () => {
@@ -81,16 +82,16 @@ export default class App extends Component {
         console.log('failed to load the sound', error)
         return
       } else {
-        s.play((success) => {
-          if (success) {
-            console.log('played it!')
+        this.setState({ music: s, isPlaying: true })
+        s.play(( success ) => {
+          if ( success ) {
+            s.release()
+            this.setState({ isPlaying: false, showAlarm: false })
           } else {
-            console.log('playback failed.')
             s.reset()
           }
         })
       }
-      console.log('duration in seconds: ' + Number(s.getDuration()).toFixed(3) + ' ')
     })
   }
 
@@ -117,7 +118,17 @@ export default class App extends Component {
     let tomorrow = moment( setdate ).format('YYYY-MM-DD')
     axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}&date=${tomorrow}`)
       .then( res => {
-          this.setState({ sunrise: res.data.results.sunrise })
+        this.setState({ sunrise: res.data.results.sunrise, showAlarm: true })
+        let offsetInHours = new Date().getTimezoneOffset() / 60
+        let setDate = moment(new Date()).add(1, 'days')
+        let dateStr = moment(setDate).format("YYYY-MM-DD")
+        let myStr = `${dateStr} ${ res.data.results.sunrise }`
+        let myMoment = moment( myStr, "YYYY-MM-DD hh:mm:ss A")
+        console.log(moment(myMoment).format())
+        let myTime = moment(myMoment).subtract(offsetInHours, 'hours')
+        console.log(moment(myTime).format())
+        this.setState({ setDate: myTime })
+        this.setAlarm( myTime )
     })
   }
 
@@ -131,46 +142,44 @@ export default class App extends Component {
     )
   }
 
-  render() {
-    const { lat, long, selection, sunrise, isSet } = this.state
-    let offsetInHours = new Date().getTimezoneOffset() / 60
-    let setdate = moment( new Date() ).add( 1, 'days' )
-    let datestr = moment( setdate ).format("YYYY-MM-DD")
-    if ( sunrise !== '' ) {
-      let mystr = `${datestr} ${sunrise}`
-      let mytime = moment( mystr, 'YYYY-MM-DD hh:mm:ss A')
-      mytime = moment( mytime ).subtract(offsetInHours, 'hours')
-      //setdate = moment( mytime ).toISOString()
-      setdate = moment(new Date()).add(10, 'seconds').toISOString()
-      setdate = moment( setdate ).toISOString()
+  cancelAlarm = ( timer, music, isPlaying ) => {
+    BackgroundTimer.clearInterval( timer )
+    Alert.alert(
+      '',
+      'Good morning!',
+      [
+        { text: '', onPress: () => null },
+      ],
+      { cancelable: false }
+    )
+    this.setState({ showAlarm: false })
+    if( isPlaying ) {
+      music.stop().release()
+      this.setState({ isPlaying: false })
     }
+  }
+
+  render() {
+    const { lat, long, showAlarm, setDate, timer, music, isPlaying } = this.state
     return (
       <View style={ styles.container }>
-        <Text style={ styles.text }>{`Alarm set: ${isSet}` }</Text>
-        { sunrise === '' ?
         <View>
-          <View>
-            <Text style={styles.title}>Sunrise alarm</Text>
-            <Text style={styles.link}
-              onPress={() => Linking.openURL('http://sunrise-sunset.org')}>
-              API by http://sunrise-sunset.org
+          <Text style={styles.title}>Sunrise Alarm</Text>
+          <Text style={styles.link}
+            onPress={() => Linking.openURL('http://sunrise-sunset.org')}>
+            API by http://sunrise-sunset.org
           </Text>
-          </View>
-          <View style={styles.container}>
-            <Picker style={styles.picker}
-              selectedValue={this.state.selection}
-              onValueChange={(selection) => this.setState({ selection })} >
-              <Picker.Item label="Set a new sunrise alarm" value="set" />
-              <Picker.Item label="Delete sunrise alarm" value="delete" />
-            </Picker>
-            <Button title="Go" onPress={() => this.handleSelection(selection, Number(lat).toFixed(7), Number(long).toFixed(7))} />
-          </View>
         </View>
+
+        { showAlarm === false ?
+            <Button title="Set a sunrise alarm" onPress={() => this.setSunriseAlarm(Number(lat).toFixed(7), Number(long).toFixed(7))} />
         :
         <View>
-          <Text style={ styles.text }>The next sunrise is at: { setdate }</Text>
-          <Button title="Set alarm" onPress={() => this.handleSetter( setdate )} />
-        </View> 
+            <Text>{`Alarm set for ${moment(setDate).format('MMMM Do YYYY, h:mm:ss a') }`}</Text>
+          <Button title="Disable the alarm" onPress={() => this.cancelAlarm( timer, music, isPlaying )} />
+          { isPlaying ? <Text>The alarm is currently playing</Text> : null }
+        </View>
+
         }
       </View>
     )
